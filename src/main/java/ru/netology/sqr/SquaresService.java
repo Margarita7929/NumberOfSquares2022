1a60170d65429ce92b651da0aa3c1d5c7885dccd
Margarita7929 <bykovskayamargarita@gmail.com>
2022-05-16 17:54:33 +0300

    commited

---
+++ src/main/java/ru/netology/sqr/SquaresService.java
@@ -0,0 +1,15 @@
+package ru.netology.sqr;
+
+public class SquaresService {
+
+    public int sqrtCount(int lowerLimit, int upperLimit) {
+        int count = 0;
+        for (int i = 4; i <= upperLimit; i++) {
+            if (i * i >= lowerLimit)
+                if (i * i <= upperLimit) {
+                    count++;
+                }
+        }
+        return count;
+    }
+}
