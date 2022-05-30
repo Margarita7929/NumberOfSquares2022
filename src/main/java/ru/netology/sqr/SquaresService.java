d7c5885a892d6b60e95c8989f92c6dc4cc06d6fb
Margarita7929 <bykovskayamargarita@gmail.com>
2022-05-30 22:17:09 +0300

    Reformat code

--- src/main/java/ru/netology/sqr/SquaresService.java
+++ src/main/java/ru/netology/sqr/SquaresService.java
@@ -5,9 +5,9 @@ public class SquaresService {
     public int sqrtCount(int lowerRangeLimit, int upperRangeLimit) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i*i >= lowerRangeLimit && i*i <= upperRangeLimit) {
-                    count++;
-                }
+            if (i * i >= lowerRangeLimit && i * i <= upperRangeLimit) {
+                count++;
+            }
         }
         return count;
     }
